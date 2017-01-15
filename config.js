6c226d018839275ed5c79ca3e4b0aa86a58ab055
Piolin Victor <159058@supinfo.com>
2017-01-15 19:42:11 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 var config = module.exports = {};
 
-config.token = "EAAKj8y1NHI4BACBZAFuBK6AHez7AGz4fbQUwxCTZAcGLopBwhl7HAAiNnMO64AtywNB0PiN5XOJedij2kPGQMhhYwhwEgx7PhTSN7TN8qOgV2wsZAd03IFl92ZCVCYqXFZBlULZBYAlTTHQHbfi9wMEY9ZCPUTfjKKULtz0pI6qiAZDZD"
-config.verify = "victor_bot"
-config.secret = "e28cc2d2c8c4dc81d21811e4f13ca34b"
+config.token = "TOKEN HERE"
+config.verify = "VERIFY HERE"
+config.secret = "SECRET HERE"
