d0d5b4808f76ee82d5f135ba78590fb680991ead
Victor Piolin <victor.piolin@gmail.com>
2017-01-15 23:01:15 +0100

    Fixed

--- app.js
+++ app.js
@@ -20,11 +20,52 @@ bot.on('message', (payload, reply) => {
 
     console.log( `Il y a ${profile.first_name} ${profile.last_name} qui me parle... Oulah, je comprend rien, je vais me coucher` );
 
-    reply({text} , (err) => {
-      if (err) throw err
+    var messageData = {
+        attachment: {
+          type: "template",
+          payload: {
+            template_type: "generic",
+            elements: [{
+              title: "rift",
+              subtitle: "Next-generation virtual reality",
+              item_url: "https://www.oculus.com/en-us/rift/",
+              image_url: "http://messengerdemo.parseapp.com/img/rift.png",
+              buttons: [{
+                type: "web_url",
+                url: "https://www.oculus.com/en-us/rift/",
+                title: "Open Web URL"
+              }, {
+                type: "postback",
+                title: "Call Postback",
+                payload: "Payload for first bubble",
+              }],
+            }, {
+              title: "touch",
+              subtitle: "Your Hands, Now in VR",
+              item_url: "https://www.oculus.com/en-us/touch/",
+              image_url: "http://messengerdemo.parseapp.com/img/touch.png",
+              buttons: [{
+                type: "web_url",
+                url: "https://www.oculus.com/en-us/touch/",
+                title: "Open Web URL"
+              }, {
+                type: "postback",
+                title: "Call Postback",
+                payload: "Payload for second bubble",
+              }]
+            }]
+          }
+        };
 
-      // console.log(`Echoed back to ${profile.first_name} ${profile.last_name}: ${text}`)
+    bot.sendMessage( payload.sender.id, messageData,  ( err, info ) => {
+      if (err) throw err
     })
+
+    // reply({text} , (err) => {
+    //   if (err) throw err
+    //
+    //   // console.log(`Echoed back to ${profile.first_name} ${profile.last_name}: ${text}`)
+    // })
   })
 })
 
