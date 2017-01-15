3bd4d87ea4189d01faa5274ba86a1a3f3e83fb6d
Victor Piolin <victor.piolin@gmail.com>
2017-01-15 23:21:39 +0100

    text dialouge

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
+var fs = require('fs')
 const http = require('http')
 const config = require( './config.js' )
 const Bot = require('messenger-bot')
+
+
+var commande = JSON.parse(fs.readFileSync('file', 'utf8'))
 
 let bot = new Bot({
   token: config.token,
@@ -20,45 +24,45 @@ bot.on('message', (payload, reply) => {
 
     console.log( `Il y a ${profile.first_name} ${profile.last_name} qui me parle... Oulah, je comprend rien, je vais me coucher` );
 
-    var messageData = {
-        attachment: {
-          type: "template",
-          payload: {
-            template_type: "generic",
-            elements: [{
-              title: "rift",
-              subtitle: "Next-generation virtual reality",
-              item_url: "https://www.oculus.com/en-us/rift/",
-              image_url: "http://messengerdemo.parseapp.com/img/rift.png",
-              buttons: [{
-                type: "web_url",
-                url: "https://www.oculus.com/en-us/rift/",
-                title: "Open Web URL"
-              }, {
-                type: "postback",
-                title: "Call Postback",
-                payload: "Payload for first bubble",
-              }],
-            }, {
-              title: "touch",
-              subtitle: "Your Hands, Now in VR",
-              item_url: "https://www.oculus.com/en-us/touch/",
-              image_url: "http://messengerdemo.parseapp.com/img/touch.png",
-              buttons: [{
-                type: "web_url",
-                url: "https://www.oculus.com/en-us/touch/",
-                title: "Open Web URL"
-              }, {
-                type: "postback",
-                title: "Call Postback",
-                payload: "Payload for second bubble",
-              }]
-            }]
-          }
-        }
-      };
+    // var messageData = {
+    //     attachment: {
+    //       type: "template",
+    //       payload: {
+    //         template_type: "generic",
+    //         elements: [{
+    //           title: "rift",
+    //           subtitle: "Next-generation virtual reality",
+    //           item_url: "https://www.oculus.com/en-us/rift/",
+    //           image_url: "http://messengerdemo.parseapp.com/img/rift.png",
+    //           buttons: [{
+    //             type: "web_url",
+    //             url: "https://www.oculus.com/en-us/rift/",
+    //             title: "Open Web URL"
+    //           }, {
+    //             type: "postback",
+    //             title: "Call Postback",
+    //             payload: "Payload for first bubble",
+    //           }],
+    //         }, {
+    //           title: "touch",
+    //           subtitle: "Your Hands, Now in VR",
+    //           item_url: "https://www.oculus.com/en-us/touch/",
+    //           image_url: "http://messengerdemo.parseapp.com/img/touch.png",
+    //           buttons: [{
+    //             type: "web_url",
+    //             url: "https://www.oculus.com/en-us/touch/",
+    //             title: "Open Web URL"
+    //           }, {
+    //             type: "postback",
+    //             title: "Call Postback",
+    //             payload: "Payload for second bubble",
+    //           }]
+    //         }]
+    //       }
+    //     }
+    //   };
 
-    bot.sendMessage( payload.sender.id, messageData,  ( err, info ) => {
+    bot.sendMessage( payload.sender.id, { text: commande.bonjour },  ( err, info ) => {
       if (err) throw err
     })
 
